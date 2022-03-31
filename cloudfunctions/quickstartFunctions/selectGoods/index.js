9ea655b6a069435b21df3d3aa26f0d960839d6a2
Lxy12138 <43946281+Lxy12138@users.noreply.github.com>
2022-04-01 00:22:54 +0800

    Update index.js
    修复了1.0的读取Goods单条记录的的只能读取第一条的问题

--- cloudfunctions/quickstartFunctions/selectGoods/index.js
+++ cloudfunctions/quickstartFunctions/selectGoods/index.js
@@ -5,13 +5,14 @@ cloud.init({
 });
 const db = cloud.database();
 
-// 查询数据库集合云函数入口函数
+// 选择商品
 exports.main = async (event, context) => {
   // 返回数据库查询结果
+  //修复了1.0的读取Goods单条记录的的只能读取第一条的问题
   if(event.event && event.event.id){
-    return await db.collection('goods').get({
+    return await db.collection('goods').where({
       _id: event.event.id
-    });
+    }).get();
   } else {
     return await db.collection('goods').where({
       is_online: true
